a7ca0153b3f28d1dccb74fe356b13b0e43c4e261
Charles Mita <mita.charles@gmail.com>
2020-12-24 01:54:01 +0100

    Improve solution to 23 (2020)
    Replace the linked list with an array mapping indices to successor
    values.
    
    The answer to "Should I use a Linked List?" is usually "No!".
    
    (Runtime 30s -> 8s)

--- 2020/23.py
+++ 2020/23.py
@@ -1,84 +1,55 @@
-class Node:
-    def __init__(self, value):
-        self.fwrd = self
-        self.back = self
-        self.value = value
-
-
-    def unlink(self):
-        self.fwrd.back = self.back
-        self.back.fwrd = self.fwrd
-        return self.fwrd
-
-
-    def link(self, node):
-        node.fwrd = self.fwrd
-        node.fwrd.back = node
-        self.fwrd = node
-        node.back = self
-        return node
-
-
-    def insert(self, value):
-        """Adds a node after the current node and returns it."""
-        node = Node(value)
-        return self.link(node)
-
-
-def create_circle(digit_str):
-    digits = map(int, (s for s in digit_str))
-    pointers = [None] * max(digits)
-    circle = Node(digits[0])
-    pointers[digits[0]-1] = circle
-    for v in digits[1:]:
-        circle = circle.insert(v)
-        pointers[v-1] = circle
-    return circle.fwrd, pointers
-
-
-def cycle(circle, pointers):
-    n1, n2, n3 = circle.fwrd, circle.fwrd.fwrd, circle.fwrd.fwrd.fwrd
-    n1.unlink()
-    n2.unlink()
-    n3.unlink()
-    v1, v2, v3 = n1.value, n2.value, n3.value
-    d = circle.value - 1
-    min_v, max_v = 1, len(pointers)
+def cycle(data, v):
+    """
+    Data is an array mapping index->next_index
+    that is, a[i] is the successor to i
+    """
+    min_v, max_v = 1, len(data) - 1
+    v1 = data[v]
+    v2 = data[v1]
+    v3 = data[v2]
+    data[v] = data[v3]
+    d = v - 1
     if d < min_v: d = max_v
     while d in [v1, v2, v3]:
         d -= 1
-        if d < min_v:
-            d = max_v
-    node = circle
-    node = pointers[d-1]
-    node.link(n1).link(n2).link(n3)
-    return circle.fwrd
+        if d < min_v: d = max_v
+    data[v3] = data[d]
+    data[d] = v1
+    return data[v]
 
 
-def collect_values(circle):
-    values = []
-    n = circle
-    while n.value != 1:
-        n = n.fwrd
-    n = n.fwrd
-    while n.value != 1:
-        values.append(n.value)
-        n = n.fwrd
-    return "".join(map(str, values))
+def array_string(data, i=1):
+    s = ""
+    for n in range(len(data) - 1):
+        s += str(i)
+        i = data[i]
+    return s
+
+
+def create_array(digits):
+    digits = map(int, (c for c in digits))
+    data = [0] * (len(digits) + 1)
+    for d, s in zip(digits[:-1], digits[1:]):
+        data[d] = s
+    data[digits[-1]] = digits[0]
+    return data
+
 
 
 TEST_INPUT = "389125467"
 INPUT = "394618527"
 
-circle, pointers = create_circle(INPUT)
+data = create_array(INPUT)
+initial_v = 3
+v = initial_v
 for n in range(100):
-    circle = cycle(circle, pointers)
-print collect_values(circle)
+    v = cycle(data, v)
+print array_string(data, 1)[1:]
 
-digits = [n+1 for n in range(10**6)]
+digits = [n+1 for n in xrange(10**6)]
 digits[:len(INPUT)] = [int(v) for v in INPUT]
-circle, pointers = create_circle(digits)
+data = create_array(digits)
+v = initial_v
 for n in range(10**7):
-    circle = cycle(circle, pointers)
-node = pointers[0]
-print node.fwrd.value * node.fwrd.fwrd.value
+    v = cycle(data, v)
+print data[1] * data[data[1]]
