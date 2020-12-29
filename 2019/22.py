19b88d7bb07ac613f17e114f0627d32603279750
Charles Mita <mita.charles@gmail.com>
2020-12-29 19:13:32 +0100

    Solve problem 22 part 2 (2019)
    Some algebra and a little bit of modular arithmetic.
    The idea is quite simple, but not initially obvious to reach.
    
    The idea is to get the affine transformation for k iterations of the
    cycle and then calculate the inverse.

--- 2019/22.py
+++ 2019/22.py
@@ -21,32 +21,82 @@ def parse_file(filename):
         return [parse_line(l) for l in f]
 
 
-def rev(cards):
-    return list(reversed(cards))
+"""
+We can view our operations as the following linear transformations modulo N
+New Deck = Mirror
+Cut n = Translate by n
+Inc n = Scale by n
+"""
+def process_transforms(commands):
+    """
+    Reduce the transforms to the form f(x) = p*x + d
+    Returns p, d
+    """
+    s = 1
+    p = 1
+    for cmd, v in commands:
+        if cmd == REV:
+            s = -s - 1
+            p *= -1
+        elif cmd == CUT:
+            s = s - v
+        elif cmd == INC:
+            s = s * v
+            p *= v
+    return p, s - 1 * p
 
 
-def cut(cards, n):
-    return cards[n:] + cards[:n]
+"""
+i1 = i0 * p + d
+
+i2 = i1 * p + d
+   = (i0 * p + d) * p + d
+   = i0*p**2 + p * d + d
+
+in = i0*p**n + d*p**(n-1) + d*p**(n-2) + ... + d*p + d
+
+sum(p**k) for k=0 to n == (p**(n+1) - 1) / (p - 1)
+in = i*p**n + d * (p**(n+1)-1) / (p-1)
+"""
+def affine_pow(p, d, k, m):
+    """
+    Calculates pk, dk for f^k(x) = pk * x + dk
+    where: f(x) = (px + d) % m
+
+    Results are modulo m
+    """
+    if k == 0:
+        return 1, 0
+    if k % 2 == 0:
+        return affine_pow(p*p % m, (p*d + d) % m, k/2, m)
+    else:
+        p2, d2 = affine_pow(p, d, k-1, m)
+        return p*p2 % m, (d2*p + d) % m
 
 
-def inc(cards, n):
-    new_cards = [0] * len(cards)
-    i = 0
-    for c in cards:
-        new_cards[i] = c
-        i += n
-        i %= len(cards)
-    return new_cards
+def mod_inv(a, m):
+    t, t2 = 0, 1
+    r, r2 = m, a
+    while r2:
+        q = r / r2
+        t, t2 = t2, t - q*t2
+        r, r2 = r2, r - q*r2
+    if r > 1: raise ValueError("No inverse")
+    if t < 0: t += m
+    return t
 
 
 commands = parse_file("22.txt")
-initial_cards = list(range(10007))
-cards = initial_cards
-for cmd, v in commands:
-    if cmd == REV:
-        cards = rev(cards)
-    elif cmd == CUT:
-        cards = cut(cards, v)
-    elif cmd == INC:
-        cards = inc(cards, v)
-print cards.index(2019)
+p, d = process_transforms(commands)
+s = 2019 * p + d
+print s % 10007
+
+L = 119315717514047
+T = 101741582076661
+p, d = affine_pow(p, d, T, L)
+"""
+2020 = X * p + d mod L
+X = p**-1 * (2020 - d) mod L
+"""
+pinv = mod_inv(p, L)
+print (2020 - d) * pinv % L
