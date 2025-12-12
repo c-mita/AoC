dc9368489f89aa1f322f8aa4d20d4ff36e5c444a
Charles Mita <cmita@google.com>
2025-12-13 00:29:56 +0100

    Improve solution for day 10 of 2025.
    An alternative approach with much better performance.
    25 minutes to 0.6 seconds - a 2500x speedup.

--- 2025/10.py
+++ 2025/10.py
@@ -1,27 +1,37 @@
 """
-Not a great solution for Part 2, but it works.
+A total rework of my original approach.
 
-For Part 1 we just BFS over the space. Switches are basically XOR operations.
-We could just consider the power set of the switches and test, which really
-ends up being the same thing. (There's no need to ever press a switch more than
-once because a xor a == 0 and xor commutes).
+For Part 1 we just test every element of the powerset of switches.
+There aren't many, this is fast. And we'll need the powerset anyway.
 
-Part 2 is an integer linear programming problem. We treat it as a recursive
-backtracking one.
+For Part 2, we no longer do a DFS search for individual button presses.
+Compared to my previous approaches, I think this is beautiful. I wish I
+could say I discovered it myself, but I encountered the basic idea first
+on reddit.
 
-We try to order switches so that we process ones that "uniquely" affect components
-of the target first. After this, we prioritise ones that affect components with large
-values.
+Consider a joltage target (j1, j2, ..., jn).
+Compute the parity of each element (p1, p2, .., pn) where (pi == ji % 2).
+This is analagous to the "lights" in Part 1.
 
-We can then guess the amount of times each switch is pressed recursively.
+For each combination of buttons "C" that lead to this parity:
+    * Apply the button presses to the target to get a new one.
+    * The elements of the new target will all be even. We can safely
+      divide every element by 2.
+    * Recurse:
+       S = recurse(new_target) * 2 + len(combination)
+Return the minimal S from the loop
 
-We can bound our guesses above and below by checking the components of the "remaining"
-target after taking into account of previous guesses.
+The recursive function can (and should) be memoized.
+We can also precompute the mapping of parities to combinations.
 
-Runtime: ~25 minutes.
+Runtime ~0.6 seconds.
+An improvement of a factor of 36,000 compared to my original 6 hour
+solution.
 """
 
 
+import collections
+import itertools
 import re
 
 
@@ -42,118 +52,68 @@ def parse_data(data):
         yield target_pattern, switches, joltages
 
 
-def toggle_switches(target, switches):
-
-    def bfs(start=0, target=0, options=()):
-        front = [start]
-        visited = set(front)
-        d = 0
-        while front:
-            next_front = set()
-            for pos in front:
-                if pos == target:
-                    return d
-                for option in options:
-                    npos = pos ^ option
-                    if npos in visited:
-                        continue
-                    next_front.add(npos)
-                    visited.add(npos)
-            front = next_front
-            d += 1
-        raise ValueError("Could not complete")
-
-    t = 0
-    # the LSB refers to the first target light
-    # reverse the light order so this lines up with our
-    # bit operations
-    for s in reversed(target):
-        t <<= 1
-        if s == "#":
-            t |= 1
-    toggles = []
-    for switch in switches:
-        toggle = 0
-        for v in switch:
-            toggle |= 1 << v
-        toggles.append(toggle)
-    return bfs(0, t, toggles)
+def powerset(elements):
+    return itertools.chain.from_iterable(
+            itertools.combinations(elements, r) for r in range(len(elements) + 1)
+    )
 
 
-def solve_switches(target, switches):
-
-    # we want to reorder the switches into an order that helps processing
-    # We want the current switch to be the one selects "unique"
-    # components in the target out of the remaining unprocessed switches.
-    # In the event of a tie, prioritise the switch that affects the
-    # component with the largest value
-    def next_switch(remaining_switches):
-        frequencies = [0] * len(target)
-        for switch in remaining_switches:
-            for s in switch:
-                frequencies[s] += 1
-        fidx, fvalue = min(((i, s) for (i, s) in enumerate(frequencies) if s > 0), key=lambda k: k[1])
-        return max((switch for switch in remaining_switches if fidx in switch), key=lambda s:max(target[i] for i in s))
-        #return max((switch for switch in remaining_switches if fidx in switch), key=len)
-
-    switches = set(switches)
-    ordered_switches = []
-    while switches:
-        switch = next_switch(switches)
-        switches.remove(switch)
-        ordered_switches.append(switch)
-
-    switches = ordered_switches
-    best_solution = 0x7FFFFFFF
-
-    # Record if, for a given target component and switch index,
-    # there are follow-up switches that affect that target component
-    # i.e. if the current switch is not the last one affecting that
-    # component
-    have_other_switches = {}
-    for idx, switch in enumerate(switches):
+def valid_combination(target, switch_set):
+    value = 0
+    for switch in switch_set:
         for s in switch:
-            have_other_switches[(s, idx)] = False
-            for j in range(idx + 1, len(switches)):
-                other = switches[j]
-                if s in other:
-                    have_other_switches[(s, idx)] = True
+            value ^= 1 << s
+    return target == value
 
 
-    def solve(switch_idx, reduced_target, presses=[]):
-        nonlocal best_solution
-        n_presses = sum(presses)
-        if n_presses >= best_solution:
-            return
-        if n_presses + max(reduced_target) >= best_solution:
-            return
-        if switch_idx == len(switches):
-            if all(v == 0 for v in reduced_target):
-                best_solution = n_presses
-                yield list(presses)
-            return
+def valid_combinations(target, switches):
+    i = 0
+    target_value = 0
+    for t in reversed(target):
+        target_value <<= 1
+        target_value |= 1 if t == "#" else 0
+    for combination in powerset(switches):
+        if valid_combination(target_value, combination):
+            yield combination
 
-        # need to work out how many times we can press the current button
-        switch = switches[switch_idx]
-        max_presses = min(reduced_target[i] for i in switch)
 
-        # how many times must we press this button?
-        min_presses = 0
-        for s in switch:
-            if not have_other_switches[(s, switch_idx)]:
-                min_presses = max(min_presses, reduced_target[s])
+def solve_joltages(target, switches):
+    cache = {}
+    parity_lookup = collections.defaultdict(list)
 
-        for n in range(max_presses, min_presses-1, -1):
-            presses.append(n)
-            new_target = list(reduced_target)
+    for combination in powerset(switches):
+        parity = [0] * len(target)
+        for switch in combination:
             for s in switch:
-                new_target[s] -= n
-            yield from solve(switch_idx + 1, new_target, presses)
-            presses.pop()
+                parity[s] ^= 1
+        parity = tuple(parity)
+        parity_lookup[parity].append(combination)
 
-    solutions = solve(0, target, [])
-    v = min(solutions, key=lambda s: sum(s))
-    return v
+    def solve(target):
+        if all(t == 0 for t in target):
+            return 0
+        if target in cache:
+            return cache[target]
+        parity = tuple(t % 2 for t in target)
+        best = 0x7FFFFFFF
+        if parity not in parity_lookup:
+            return best
+        for combination in parity_lookup[parity]:
+            new_target = list(target)
+            for switch in combination:
+                for c in switch:
+                    new_target[c] -= 1
+            if any(t < 0 for t in new_target):
+                continue
+            # every target element is now even, so we can halve
+            # it and just multiply the result of searching by two
+            new_target = tuple(t // 2 for t in new_target)
+            # we pressed len(combination) buttons to reach here
+            solution = solve(new_target) * 2 + len(combination)
+            best = min(best, solution)
+        cache[target] = best
+        return best
+    return solve(target)
 
 
 TEST_DATA = """
@@ -166,16 +126,14 @@ test_entries = list(parse_data(TEST_DATA))
 with open("10.txt") as f:
     entries = list(parse_data(f))
 
-s = 0
-for target, switches, joltages in entries:
-    s += (toggle_switches(target, switches))
-print(s)
-
 #entries = test_entries
 s = 0
 for target, switches, joltages in entries:
-    solution = solve_switches(joltages, switches)
-    print(solution)
-    s += sum(solution)
+    s += min(len(combination) for combination in valid_combinations(target, switches))
 print(s)
 
+
+s = 0
+for target, switches, joltages in entries:
+    s += solve_joltages(joltages, switches)
+print(s)
